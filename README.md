81843a4e98b7c535183b8c2cc6fcdd6c53ce2c92
Tomas Karasek <tom.to.the.k@gmail.com>
2017-04-12 18:03:30 +0300

    i2

---
+++ README.md
@@ -0,0 +1,106 @@
+# Complete CLI for Kraken in Python 3
+
+I tried to take https://www.kraken.com/help/api and express it in Python fucntion declaration. The functions are crunched by argh[0] and exposed to argument parser, rendering a relatively nice CLI which very closely follows the Kraken API doc.
+
+In general, compulsory arguments are positional, and optional/reasonable-defaults arguments are passed by flag. E.g. to display order book of XXBTZEUR up to 15 items: `./kx Depth -c 15 XXBTZEUR`.
+
+I wrote this because the other CLIs to Kraken seemed too complicated. This CLI is one file and has relatively few and standard dependencies.
+
+[0] https://github.com/neithere/argh/
+
+## Dependencies
+
+`pip3 install requests tabulate argh`
+
+If you want autocomplete in bash, you must install argcomplete:
+
+```
+pip3 install argcomplete
+activate-global-python-argcomplete
+```
+
+## Usage
+
+You can do whatever is at https://www.kraken.com/help/api. If unsure, see `./kx help <Method>`, e.g. `./kx help AddOrder`
+
+### Authentication
+
+If you want to use private API calls, you need to get API key and secret from https://www.kraken.com. Put it to a file on separate lines, first key and then secret, e.g.
+
+```
+xvjiddoff0942r029rj340r43GFDgDFgdfgdfgfdgDFGFDgdfgd46868
+TgfopdgkdfGSFdg54$5gf45g+gbrgrtHY5imlkm98htfghdfh4h+516+fdh+g4hfgh+fg6hdfhdfhdf+gdfghD==
+```
+
+and then export path to this file in environment variable `KRAKENKEY_FILE`.
+
+### One time passwords
+
+In Kraken, you can set OTP for a particular API keys. You can even specify for which method groups you want to use OTP (Depositing, Withdrawing, Creating orders, Closing positions, Canceling orders).
+
+You can istruct this scipt to use OTP. Just set env var `KRAKENAPI_USE_OTP` to a true-evaluating string. The script will then ask you for OTP, and will cache it for 90 seconds. 
+
+### Examples
+
+Show available assets.
+
+```
+./kx Assets
+```
+
+Show asset pairs
+
+```
+./kx AssetPairs
+```
+
+Show balance (private method), and ask for OTP:
+
+```
+KRAKENAPI_USE_OTP=1 ./kx Balance
+```
+
+Sell 0.001 BTC for EUR 
+
+```
+export KRAKENAPI_USE_OTP=1
+
+# first just validate the order 
+./kx AddOrder -v XXBTZEUR sell market 0.001
+
+# If OK, then submit it
+./kx AddOrder XXBTZEUR sell market 0.001
+```
+
+Place limit order to buy 0.1 BTC for 931.14 EUR
+
+```
+./kx AddOrder -p 931.14 XXBTZEUR buy limit 0.1
+```
+
+### Tips
+
+- `AddOrder` has `-v` flag which will only validate the order, it will not create it.
+
+### Using as a module
+
+The HTTP connection to kraken API is cached within a process via requests.Session. Thus the TCP/SSL connection is opened only once for consequent requests in the same script.
+
+It appears that the largest delay is in opening the connection. Once that's over, the API requests are quite fast.
+
+You can call the functions from a Python script and process the results yourself if you want. You just need to tell `kraken_cli.py` not to print the response by setting the `KRAKENCLI_RAW_RESULT` env var to true-evaluating string.
+
+```
+import kraken_cli as kx
+import os
+
+os.environ['KRAKENCLI_RAW_RESULT'] = "yeah"
+os.environ['KRAKENAPI_USE_OTP'] = "yeah"
+
+b1 = kx.Balance()
+```
+
+## TODO
+
+- Nice shell completion in zsh/oh-my-zsh. Is there sth that could work with argh??
+- Wrappers around AddOrder to sanitize the more common limit orders.
---
+++ kraken_cli.py
@@ -0,0 +1,550 @@
+#!/usr/bin/env python3
+
+# All you need to use https://www.kraken.com/help/api in one file with a lot of
+# dependencies.
+
+# PYTHON_ARGCOMPLETE_OK
+
+import os
+import sys
+import copy
+import time
+import calendar
+import pprint
+import logging
+import getpass
+import datetime
+import urllib
+import functools
+import dateutil.parser
+import hashlib
+import hmac
+import base64
+
+import argh
+import requests
+import tabulate
+
+KRAKEN_API_URL = "https://api.kraken.com"
+
+
+OTP_CACHE_FILE = os.path.expanduser("~/.kraken_otp")
+
+
+KRAKENKEY_FILE_ENV_VAR = "KRAKENKEY_FILE"
+
+
+ASSETS = ['KFEE', 'USDT', 'XDAO', 'XETC', 'XETH', 'XICN', 'XLTC', 'XMLN',
+          'XNMC', 'XREP', 'XXBT', 'XXDG', 'XXLM', 'XXMR', 'XXRP', 'XXVN',
+          'XZEC', 'ZCAD', 'ZEUR', 'ZGBP', 'ZJPY', 'ZKRW', 'ZUSD', 'EUR',
+          'FEE', 'USDT', 'DAO', 'ETC', 'ETH', 'ICN', 'LTC', 'MLN', 'NMC',
+          'REP', 'XBT', 'XDG', 'XLM', 'XMR', 'XRP', 'XVN', 'ZEC', 'CAD',
+          'GBP', 'JPY', 'KRW', 'USD']
+
+
+PAIRS = ['USDTZUSD', 'XETCXETH', 'XETCXXBT', 'XETCZEUR', 'XETCZUSD',
+         'XETHXXBT', 'XETHXXBT.d', 'XETHZCAD', 'XETHZCAD.d', 'XETHZEUR',
+         'XETHZEUR.d',
+         'XETHZGBP', 'XETHZGBP.d', 'XETHZJPY', 'XETHZJPY.d', 'XETHZUSD',
+         'XETHZUSD.d', 'XICNXETH', 'XICNXXBT', 'XLTCXXBT', 'XLTCZEUR',
+         'XLTCZUSD', 'XMLNXETH', 'XMLNXXBT', 'XREPXETH', 'XREPXXBT',
+         'XREPZEUR',
+         'XREPZUSD', 'XXBTZCAD', 'XXBTZCAD.d', 'XXBTZEUR', 'XXBTZEUR.d',
+         'XXBTZGBP', 'XXBTZGBP.d', 'XXBTZJPY', 'XXBTZJPY.d', 'XXBTZUSD',
+         'XXBTZUSD.d', 'XXDGXXBT', 'XXLMXXBT', 'XXLMZEUR', 'XXLMZUSD',
+         'XXMRXXBT', 'XXMRZEUR', 'XXMRZUSD', 'XXRPXXBT', 'XZECXXBT',
+         'XZECZEUR',
+         'XZECZUSD', 'USDTUSD', 'ETCETH', 'ETCXBT', 'ETCEUR', 'ETCUSD',
+         'ETHXBT', 'ETHXBT.d', 'ETHCAD', 'ETHCAD.d', 'ETHEUR', 'ETHEUR.d',
+         'ETHGBP', 'ETHGBP.d', 'ETHJPY', 'ETHJPY.d', 'ETHUSD', 'ETHUSD.d',
+         'ICNETH', 'ICNXBT', 'LTCXBT', 'LTCEUR', 'LTCUSD', 'MLNETH', 'MLNXBT',
+         'REPETH', 'REPXBT', 'REPEUR', 'REPUSD', 'XBTCAD', 'XBTCAD.d',
+         'XBTEUR',
+         'XBTEUR.d', 'XBTGBP', 'XBTGBP.d', 'XBTJPY', 'XBTJPY.d', 'XBTUSD',
+         'XBTUSD.d', 'XDGXBT', 'XLMXBT', 'XLMEUR', 'XLMUSD', 'XMRXBT',
+         'XMREUR',
+         'XMRUSD', 'XRPXBT', 'ZECXBT', 'ZECEUR', 'ZECUSD']
+
+
+CLOSETIMES = ['open', 'close', 'both']
+
+
+TRADETYPES = ["all", "any position", "closed position", "closing position",
+              "no position"]
+
+
+LEDGERTYPES = ['all', 'deposit', 'withdrawal', 'trade', 'margin']
+
+
+BSTYPES = ['buy', 'sell']
+
+
+ORDERTYPES = ['market', 'limit', 'stop-loss', 'take-profit',
+              'stop-loss-profit', 'stop-loss-profit-limit', 'stop-loss-limit',
+              'take-profit-limit', 'trailing-stop', 'trailing-stop-limit',
+              'stop-loss-and-limit', 'settle-position']
+
+
+def prettytime(ts):
+    dt = datetime.datetime.utcfromtimestamp(float(ts))
+    return dt.isoformat()[:22].replace("T", "_")
+
+
+def prettify_time_in_list_of_dicts(l):
+    _l = copy.deepcopy(l)
+    for i in _l:
+        for k in i:
+            if 'time' in k:
+                i[k] = prettytime(i[k])
+    return _l
+
+
+def tabprint(l, sortkey='time'):
+    _l = list(l)
+    if not _l:
+        print("empty response")
+        return
+    if sortkey in _l[0]:
+        _l = sorted(_l, key=lambda i: i[sortkey])
+    tab = prettify_time_in_list_of_dicts(_l)
+    print(tabulate.tabulate(tab, headers='keys'))
+
+
+def ohlcprint(d, pair):
+    l = d[pair]
+    # <time>, <open>, <high>, <low>, <close>, <vwap>, <volume>, <count>
+    l = [(prettytime(t), o, h, l, c, vw, v, c)
+         for t, o, h, l, c, vw, v, c in l]
+    keys = ['time', 'open', 'high', 'low', 'close', 'vwap', 'volume', 'count']
+    print(tabulate.tabulate(l, headers=keys))
+    if 'last' in d:
+        print("Last: %d, %s" % (d['last'], prettytime(d['last'])))
+
+
+def spreadprint(d, pair):
+    l = d[pair]
+    l = [(prettytime(ti), float(t) - float(b), float(b), float(t))
+         for ti, b, t in l]
+    # price, volume, time
+    keys = ['time', 'diff', 'bottom', 'top']
+    print(tabulate.tabulate(l, headers=keys))
+    if 'last' in d:
+        print("Last: %d, %s" % (d['last'], prettytime(d['last'])))
+
+
+def tradesprint(d, pair):
+    l = d[pair]
+    # price, volume, time
+    keys = ['price', 'volume', 'time', 'buy/sell', 'lim/mark', 'note']
+    l = [(p, v, prettytime(t), bs, lm, n) for p, v, t, bs, lm, n in l]
+    print(tabulate.tabulate(l, headers=keys))
+    if 'last' in d:
+        ts = int(d['last']) / 10e8
+        print("Last: %d, %s" % (int(d['last']), prettytime(ts)))
+
+
+def obprint(d, pair):
+    d = d[pair]
+    rasks = sorted(d['asks'], reverse=True)
+    rasks = [(p, v, prettytime(t)) for p, v, t in rasks]
+    bids = [(p, v, prettytime(t)) for p, v, t in d['bids']]
+    keys = ['price', 'volume', 'timestamp']
+    print(tabulate.tabulate(rasks, headers=keys))
+    print("------")
+    print("ASKS ^")
+    print("------")
+    print("BIDS v")
+    print("------")
+    print(tabulate.tabulate(bids, headers=keys))
+
+
+def guessprint(d):
+    _d = dict(d)
+    remove_keys = ['lot_decimals', 'fees', 'fee_volume_currency',
+                   'aclass_quote', 'aclass_base', 'fees_maker']
+    for k in d:
+        _d[k]['asset'] = k
+        for l in remove_keys:
+            if l in _d[k]:
+                del _d[k][l]
+    tabprint(_d.values())
+
+
+def dictprint(d):
+    _d = dict(d)
+    for k in d:
+        _d[k]['id'] = k
+    tabprint(_d.values())
+
+
+def toughdictprint(d, key):
+    if key == 'closed':
+        key = 'close'
+    _d = {k: v['descr'] for k, v in d.items()}
+    for k in _d:
+        _d[k]['ordid'] = k
+        if _d[k]['ordertype'] == 'market':
+            _d[k]['price'] = d[k]['price']
+        _d[k][key + 'time'] = d[k][key + 'tm']
+    tabprint(_d.values(), key + 'time')
+
+
+class HTTPSession(object):
+    __instance = None
+
+    def __new__(cls):
+        if HTTPSession.__instance is None:
+            s = requests.Session()
+            HTTPSession.__instance = s
+        return HTTPSession.__instance
+
+
+def iso_to_unix(isodatetime):
+    d = dateutil.parser.parse(isodatetime)
+    ts = calendar.timegm(d.timetuple())
+    return int(ts)
+
+
+def api_query_public(api_method, params):
+    urlbase = KRAKEN_API_URL + "/0/public/"
+    r = HTTPSession().get(urlbase + api_method, params=params)
+    return r.json()
+
+
+def get_otp(prompt):
+    if os.path.isfile(OTP_CACHE_FILE):
+        if os.path.getmtime(OTP_CACHE_FILE) > time.time() - 90:
+            with open(OTP_CACHE_FILE) as f:
+                return f.read()
+    otp = getpass.getpass(prompt)
+    with open(OTP_CACHE_FILE, "w") as f:
+        f.write(otp)
+    os.chmod(OTP_CACHE_FILE, 0o600)
+    return otp
+
+
+def api_query_private(api_method, params):
+    if os.environ.get("KRAKENAPI_USE_OTP"):
+        params['otp'] = get_otp("OTP for %s, params %s: " %
+                                (api_method, params))
+    urlpath = "/0/private/" + api_method
+    key_file_path = os.environ.get(KRAKENKEY_FILE_ENV_VAR)
+    if key_file_path:
+        with open(key_file_path, 'r') as f:
+            key = f.readline().strip()
+            secret = f.readline().strip()
+    else:
+        msg = ("To access private API, you need to download API key and"
+               " secret from kraken.com, and put it to a file, specified in"
+               " the %s environment variable. Put the key on first line,"
+               " and the secret on second line" % KRAKENKEY_FILE_ENV_VAR)
+        raise Exception(msg)
+
+    # based on
+    # https://github.com/veox/python3-krakenex/tree/master/krakenex/api.py
+    params['nonce'] = int(1000 * time.time())
+    postdata = urllib.parse.urlencode(params)
+    encoded = (str(params['nonce']) + postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded).digest()
+    signature = hmac.new(base64.b64decode(secret),
+                         message, hashlib.sha512)
+    sigdigest = base64.b64encode(signature.digest())
+
+    headers = {
+        'API-Key': key,
+        'API-Sign': sigdigest.decode()
+    }
+
+    r = HTTPSession().post(KRAKEN_API_URL + urlpath, data=params,
+                           headers=headers)
+    return r.json()
+
+
+def query(qtype, api_method, params={}):
+    if os.environ.get("DEBUG"):
+        logging.basicConfig(level=logging.DEBUG)
+    assert(qtype in ['private', 'public'])
+    if params.get('start'):
+        params['start'] = iso_to_unix(params['start'])
+    if params.get('end'):
+        params['end'] = iso_to_unix(params['end'])
+    if params.get('since'):
+        try:
+            params['since'] = iso_to_unix(params['since'].replace("_", "T"))
+            if api_method == 'Trades':
+                params['since'] = int(params['since'] * 10e8)
+        except Exception as e:
+            logging.debug(e)
+
+    logging.debug("sending")
+    logging.debug(api_method)
+    logging.debug(params)
+    new_params = {k.replace("_", "-"): v for (k, v) in params.items()}
+    if qtype == 'private':
+        res = api_query_private(api_method, new_params)
+    else:
+        res = api_query_public(api_method, new_params)
+    logging.debug("obtained")
+    logging.debug(res)
+
+    if 'error' in res:
+        if res['error']:
+            msg = ", ".join(res['error'])
+            if 'EAPI:Invalid signature' in res['error']:
+                msg += ", perhaps a missing/wrong/old OTP?"
+            if 'EAPI:Invalid key' in res['error']:
+                msg += (", perhaps you try to call private API method and you"
+                        " did not export path to your key file to env var %s"
+                        " or your key has expired" % KRAKENKEY_FILE_ENV_VAR)
+            raise Exception(msg)
+
+    if 'result' in res:
+        result = res['result']
+        if os.environ.get("KRAKENCLI_RAW_RESULT"):
+            return result
+        if api_method == 'Depth':
+            obprint(result, pair=params['pair'])
+        elif api_method == 'OHLC':
+            ohlcprint(result, pair=params['pair'])
+        elif api_method == 'Trades':
+            tradesprint(result, pair=params['pair'])
+        elif api_method == 'Spread':
+            spreadprint(result, pair=params['pair'])
+        elif api_method == 'Assets':
+            dictprint(result)
+        elif api_method == 'AssetPairs':
+            guessprint(result)
+        elif api_method == 'Ledgers':
+            dictprint(result['ledger'])
+        elif api_method == 'TradesHistory':
+            dictprint(result['trades'])
+        elif api_method == 'OpenOrders':
+            toughdictprint(result['open'], 'open')
+        elif api_method == 'ClosedOrders':
+            # not a typo ...
+            toughdictprint(result['closed'], 'close')
+        elif isinstance(result, list):
+            if result == []:
+                print("Got empty list as a result.")
+            else:
+                tabprint(result)
+        else:
+            pprint.pprint(res['result'])
+
+
+def set_values(_d):
+    return {k: v for k, v in _d.items()
+            if v is not None and not k.startswith("_")}
+
+
+@argh.arg("-a", "--asset", choices=ASSETS + [None])
+def Assets(info=None, aclass=None, asset=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("-i", "--info", choices=['info', 'leverage', 'fees', 'margin'])
+@argh.arg("-p", "--pair", choices=PAIRS + [None])
+def AssetPairs(info='info', pair=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+def Ticker(pair):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+def Depth(pair, count:int=20):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+@argh.arg("-i", "--interval", choices=[1, 5, 15, 30, 60, 240, 10080, 21600])
+def OHLC(pair, interval=1, since=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+def Trades(pair, since=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+def Spread(pair, since=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname, set_values(locals()))
+
+
+def Time():
+    _fname = sys._getframe().f_code.co_name
+    return query('public', _fname)
+
+
+@argh.aliases('balance', 'bal')
+def Balance():
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname)
+
+
+@argh.arg("asset", choices=ASSETS)
+def TradeBalance(asset, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+def OpenOrders(trades=None, userref=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("-c", "--closetime", choices=CLOSETIMES + [None])
+def ClosedOrders(trades=None, userref=None, start=None, end=None, ofs=None,
+                 closetime='both'):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+# txid is e.g. OZMY26-UBBFP-BFQOFG[,OYQ373-PCW3O-GKM4PM] ...
+def QueryOrders(txid, trades=None, userref=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+def QueryTrades(txid, trades=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+# Ledger id is e.g. LZMY26-UBBFP-BFQOFG[,LYQ373-PCW3O-GKM4PM] ...
+def QueryLedgers(id):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+def OpenPositions(txid=None, docalcs=False):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("-t", "--type", choices=TRADETYPES + [None])
+def TradesHistory(type=None, start=None, end=None, ofs=None,
+                  trades=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("-t", "--type", choices=LEDGERTYPES + [None])
+@argh.arg("-a", "--asset", choices=ASSETS + [None])
+def Ledgers(asset=None, type=None, aclass=None, start=None, end=None,
+            ofs=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("pair", choices=PAIRS)
+def TradeVolume(pair, fee_info=False):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+# oflags can be a list of
+#   viqc = volume in quote currency (not available for leveraged orders)
+#   fcib = prefer fee in base currency
+#   fciq = prefer fee in quote currency
+#   nompp = no market price protection
+#   post = post only order (available when ordertype = limit)
+#
+# starttm = scheduled start time (optional):
+#    0 = now (default)
+#    +<n> = schedule start time <n> seconds from now
+#    <n> = unix timestamp of start time
+#
+# expiretm = expiration time (optional):
+#    0 = no expiration (default)
+#    +<n> = expire <n> seconds from now
+#    <n> = unix timestamp of expiration time
+#
+# userref = user reference id.  32-bit signed number.  (optional)
+#
+# validate = validate inputs only.  do not submit order (optional)
+
+@argh.arg("pair", choices=PAIRS)
+@argh.arg("type", choices=BSTYPES)
+@argh.arg("ordertype", choices=ORDERTYPES)
+@argh.arg("-v", "--validate")
+@argh.arg("-p", "--price")
+@argh.arg("-p2", "--price2")
+def AddOrder(pair, type, ordertype, volume, price=None, price2=None,
+             leverage=None, oflags=None, starttm=None, expiretm=None,
+             userref=None, validate=False):
+    if not validate:
+        validate = None
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+def CancelOrder(txid):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def DepositMethods(asset, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+# method is 'Bitcoin', or ???
+@argh.arg("asset", choices=ASSETS)
+def DepositAddresses(asset, method, aclass=None, new=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def DepositStatus(asset, method, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def WithdrawInfo(asset, key, amount: float, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def Withdraw(asset, key, amount: float, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def WithdrawStatus(asset, method=None, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+@argh.arg("asset", choices=ASSETS)
+def WithdrawCancel(asset, refid, aclass=None):
+    _fname = sys._getframe().f_code.co_name
+    return query('private', _fname, set_values(locals()))
+
+
+if __name__ == '__main__':
+    exposed = [Time, Assets, AssetPairs, Ticker, OHLC, Depth, Trades,
+               Spread, Balance, TradeBalance, OpenOrders, ClosedOrders,
+               QueryOrders, TradesHistory, QueryTrades, OpenPositions,
+               Ledgers, QueryLedgers, TradeVolume, AddOrder, DepositMethods,
+               DepositAddresses, DepositStatus, WithdrawInfo, Withdraw,
+               WithdrawStatus, WithdrawCancel, CancelOrder]
+
+    parser = argh.ArghParser()
+    parser.add_commands(exposed)
+    parser.dispatch()
